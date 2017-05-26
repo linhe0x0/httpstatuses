4505f9b04b972352676be4c660096d0ab0e1c74a
sqrtthree <imsqrtthree@gmail.com>
2017-05-26 23:58:07 +0800

    Fix CI error

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ const token = process.env.GH_TOKEN
 if (!token) return console.error(new Error('Invalid token.'))
 
 ghpages.publish('build', {
-  repo: `https://${GH_TOKEN}@github.com/sqrthree/httpstatuses.git`
+  repo: `https://${token}@github.com/sqrthree/httpstatuses.git`
 }, () => {
   console.log('Published.')
 })
